770c2ef464a46dc0c94aa7ea5f753c5eae2c84c1
Roman <114938872+RBychin@users.noreply.github.com>
2023-11-25 13:56:48 +0300

    refactoring html structure

--- project/web/views.py
+++ project/web/views.py
@@ -1,6 +1,4 @@
-from datetime import datetime
 from django.shortcuts import redirect, get_object_or_404
-from django.utils import timezone
 from django.views.generic import ListView, DetailView, CreateView
 
 from core.models import Event
